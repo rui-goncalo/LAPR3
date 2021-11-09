6da7623ff1be9de797165a86862f56e6e0efc266
1180590 <1180590@isep.ipp.pt>
2021-11-09 16:08:49 +0000

    finished compareTo for doubles

--- src/main/java/lapr/project/controller/TopShipsController.java
+++ src/main/java/lapr/project/controller/TopShipsController.java
@@ -13,20 +13,23 @@ public class TopShipsController {
     protected class ShipByDistance implements Comparable<ShipByDistance>{
         
         private Ship ship;
-        private int traveledDistance;
+        private double traveledDistance;
         
-        public ShipByDistance(Ship ship, int traveledDistance) {
+        public ShipByDistance(Ship ship, double traveledDistance) {
           this.ship = ship;
           this.traveledDistance = traveledDistance;
         }
         
         // accessor methods
         public Ship getShip() { return ship; }
-        public int getTraveledDistance() { return traveledDistance; }
+        public double getTraveledDistance() { return traveledDistance; }
         
         //rever traveled distance int?
+        @Override
         public int compareTo(ShipByDistance ship){
-            return traveledDistance - ship.traveledDistance;
+            if(traveledDistance < ship.traveledDistance){return -1;}
+            if(traveledDistance > ship.traveledDistance){return 1;}
+            return 0
         }
       }
     
