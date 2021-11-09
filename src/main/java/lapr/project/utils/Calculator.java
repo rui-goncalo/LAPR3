262fa8dc9a68b6e3f491543244bda7d85c9243b0
Rui Gonçalo Sil Gonçalves <1191831@isep.ipp.pt>
2021-11-09 12:26:19 +0000

    Import Calculator class

---
+++ src/main/java/lapr/project/utils/Calculator.java
@@ -0,0 +1,34 @@
+package lapr.project.utils;
+
+import lapr.project.model.ShipData;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    //1Degree of latitude to Km   
+    private static final double DEGREE_TO_KM= 111;
+    // REVER: assumindo distances como ints
+    
+    private static double degToRad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+    
+    private static double radToDeg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+    
+    public static double distanceBetween(double lat1, double lon1, double lat2, double lon2){
+        double theta = lon1 - lon2;
+        double dist = Math.sin(degToRad(lat1)) * Math.sin(degToRad(lat2)) + Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * Math.cos(degToRad(theta));
+        dist = Math.acos(dist);
+        dist = radToDeg(dist);
+        dist = dist * DEGREE_TO_KM;
+        return dist;
+    }
+    
+    //TODO
+    public static int totalDistance(ArrayList<ShipData> shipData){
+        return 0;
+    }
+    
+}
