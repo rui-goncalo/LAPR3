513441cc9d95fc675c5f9a579ade5555b9ee72a4
Rui Gonçalo Sil Gonçalves <1191831@isep.ipp.pt>
2021-11-30 08:34:53 +0000

    G103-40 - Connection to database done

---
+++ src/main/java/lapr/project/data/MakeDBConnection.java
@@ -0,0 +1,37 @@
+package lapr.project.data;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class MakeDBConnection {
+
+    public static Connection makeConnection() {
+        try (
+                InputStream inputStream = MakeDBConnection.class.getClassLoader().getResourceAsStream("application.properties")) {
+            Properties properties = new Properties(System.getProperties());
+            properties.load(inputStream);
+            System.setProperties(properties);
+        } catch (
+                IOException e) {
+            System.out.println("Failed initialize inputStream for application.properties: " + e);
+        }
+
+        String url = System.getProperty("database.url");
+        String username = System.getProperty("database.username");
+        String password = System.getProperty("database.password");
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            connection.setAutoCommit(false);
+            return connection;
+        } catch (
+                SQLException e) {
+            System.out.println("Failed to connect to the database: " + e);
+            return null;
+        }
+    }
+}
