8b2cf439802b8d425913d3683fba68175f3da407
Tiago Pinto <1200626@isep.ipp.pt>
2022-01-22 17:11:32 +0000

    <G103-132> Updated older User Story

--- docs/Sprint 3/BDDAD/US309/US309.sql
+++ docs/Sprint 3/BDDAD/US309/US309.sql
@@ -9,14 +9,11 @@ BEGIN
     FROM SHIP S INNER JOIN VEHICLE V ON S.VEHICLEID = V.ID
                 INNER JOIN TRIP T ON S.MMSI = T.SHIPMMSI
                 INNER JOIN CARGO_MANIFEST CM ON V.ID = CM.VEHICLEID
-    WHERE S.MMSI = 211331613 AND SYSDATE BETWEEN T.INITIAL_DATE AND T.FINAL_DATE;
-
+    WHERE S.VEHICLEID = :new.vehicleid AND SYSDATE BETWEEN T.INITIAL_DATE AND T.FINAL_DATE;
     IF v_ship_on_trip > 0 THEN
         RAISE ex_ship_on_trip;
     END IF;
-
     EXCEPTION
         WHEN ex_ship_on_trip THEN
-            raise_application_error(-2000, 'Ship is on a trip');
-
+            raise_application_error(-20000, 'Ship is on a trip');
 END trg_ship_on_trip;
